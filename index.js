b2750c13e8f9936ddb7caff31f233519dbefa340
dc <duocchu9911tn@gmail.com>
2022-11-07 21:58:03 +0700

    hihihi

---
+++ index.js
@@ -0,0 +1,22 @@
+/** bài tập 1
+ * 
+ * input: nhập điểm chuẩn
+ *        điểm 3 môn của thí sinh
+ *        đối tượng ưu tiên
+ *        khu vực ưu tiên
+ * 
+ * progess: xử lý kết quả dự thi
+ *          log kqua ra màn hình
+ * 
+ * 
+ * ouput:   in kết quả
+ * 
+ */
+function ex1() {
+    document.getElementById("tinhTong").value;
+    
+    var benchmark=30;
+    
+
+}
+
